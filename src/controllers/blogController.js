2fd8283115a1bbd11b4fce7d07e954b13b041626
koraykoksal <kksl.koray@gmail.com>
2023-10-05 20:26:37 +0300

    changed postId and categoryId

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -74,7 +74,7 @@ module.exports.BlogCategory={
     read: async (req, res) => {
         // req.params.postId
         // const data = await BlogPost.findById(req.params.postId)
-        const data = await BlogCategory.findOne({ _id: req.params.postId })
+        const data = await BlogCategory.findOne({ _id: req.params.categoryId })
 
         res.status(200).send({
             error: false,
@@ -84,7 +84,7 @@ module.exports.BlogCategory={
 
     update: async (req, res) => {
         // güncelleme yapmak için req.params.postId bilgini ve req.body bilgisini belirtmek gerekir
-        const data = await BlogCategory.updateOne({_id:req.params.postId},req.body)
+        const data = await BlogCategory.updateOne({_id:req.params.categoryId},req.body)
         res.status(202).send({
             error:false,
             body:req.body,
@@ -93,7 +93,7 @@ module.exports.BlogCategory={
     },
 
     delete: async (req, res) => {
-        const data = await BlogCategory.deleteOne({ _id: req.params.postId })
+        const data = await BlogCategory.deleteOne({ _id: req.params.categoryId })
 
         res.sendStatus( (data.deletedCount >= 1) ? 204 : 404 )
     },
